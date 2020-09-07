8a5f94f75492956f46c9fefdefa9ec8259dc9695
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2020-09-07 21:31:03 +0100

    Delete SQL Value List.sql

--- SQL Value List.sql
+++
@@ -1,9 +0,0 @@
-/*IF @columnlist is not null
- begin
-   SELECT @columnlist= COALESCE(@columnlist + ', ', '') + 
-   CAST(keyname AS varchar(100))
-   FROM OPENXML (@idoc, '/root/structure',1)
-   WITH (keyname varchar(150))
-
-  SELECT @columnlist
- end  */
